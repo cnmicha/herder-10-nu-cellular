9407398b3596866697d3856275ce642189be3c08
cnmicha <evsc@gmx.de>
2015-03-26 23:03:50 +0100

    small improvements

--- automat.pas
+++ automat.pas
@@ -11,11 +11,11 @@ type
   TZustand = 0..1;
 
   TAutomat = class
-    altGen: array[0..255] of TZustand;
-    neueGen: array[0..255] of TZustand;
+    altGen: array of TZustand;
+    neueGen: array of TZustand;
     nummer: byte;
-    constructor Create;
-  procedure Create(nummmer,length:cardinal);
+    constructor Create; overload;
+    procedure Create(nummmer: byte; length: cardinal); overload;
     procedure startgeneration;
     procedure neue_gen_berechnen;
   end;
@@ -26,20 +26,22 @@ implementation
 
 constructor TAutomat.Create;
 begin
-     self.nummer := 0;
-     Randomize;
+  self.nummer := 0;
+  Randomize;
 end;
 
-procedure TAutomat.Create(nummmer,length:cardinal);
+procedure TAutomat.Create(nummmer: byte; length: cardinal);
 begin
-
+  self.nummer := nummer;
+  setLength(self.alteGen, length);
+  setLength(self.neueGen, length);
 end;
 
 procedure TAutomat.startgeneration;
 var
   i: cardinal;
 begin
-  for i := 0 to Length(self.neueGen)-1 do
+  for i := 0 to Length(self.neueGen) - 1 do
   begin
     self.neueGen[i] := Random(2);
 
@@ -49,16 +51,20 @@ end;
 
 procedure TAutomat.neue_gen_berechnen;
 var
-  i: cardinal;                                                   t
+  i: cardinal;
 begin
   self.altGen := self.neueGen;
 
-  for i := 0 to Length(self.neueGen)-1 do
-  begin
+  //todo Nummer zurückrechnen
 
+  for i := 0 to Length(self.neueGen) - 1 do
+  begin
+    // i-1: length mod (i-1)
 
     ShowMessage(IntToStr(self.neueGen[i]));
   end;
 end;
 
 end.
+
+
